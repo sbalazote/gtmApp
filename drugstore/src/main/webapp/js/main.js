bbcbadacb92ff7b2606c1ed45707ab03dd5b2fb4
luchopal <luchopal@gmail.com>
2015-07-02 19:20:06 -0300

    Se corrige el orden del tipo de documento.

--- drugstore/src/main/webapp/js/main.js
+++ drugstore/src/main/webapp/js/main.js
@@ -382,4 +382,17 @@ $(document).ready(function() {
 			}
 		}, 1000);
 	}
+
+	getURLParameter = function(sParam) {
+		var sPageURL = window.location.search.substring(1);
+		var sURLVariables = sPageURL.split('&');
+		for (var i = 0; i < sURLVariables.length; i++)
+		{
+			var sParameterName = sURLVariables[i].split('=');
+			if (sParameterName[0] == sParam)
+			{
+				return sParameterName[1];
+			}
+		}
+	}
 });
