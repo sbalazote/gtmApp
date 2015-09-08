14f51e5a00cee03e1e928c75558d0886857ab849
luchopal <luchopal@gmail.com>
2015-09-08 15:32:54 -0300

    Script de base de datos para perfiles

---
+++ drugstore/src/main/resources/db/Alter User Table.sql
@@ -0,0 +1,22 @@
+CREATE TABLE `drugstore`.`profile` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT ,
+  `description` VARCHAR(45) NULL,
+  PRIMARY KEY (`id`));
+  
+DROP TABLE `drugstore`.`user_role`;
+
+CREATE TABLE `drugstore`.`profile_role` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `profile_id` int(11) NOT NULL,
+  `role_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_profile_role_profile_idx` (`profile_id`),
+  KEY `fk_user_role_role_idx` (`role_id`),
+  CONSTRAINT `fk_profile_role_profile` FOREIGN KEY (`profile_id`) REFERENCES `profile` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_profile_role_role` FOREIGN KEY (`role_id`) REFERENCES `role` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+  ALTER TABLE `drugstore`.`user`
+  ADD COLUMN `profile_id` int(11),
+  ADD KEY `fk_user_profile_idx` (`profile_id`),
+  ADD CONSTRAINT `fk_user_profile` FOREIGN KEY (`profile_id`) REFERENCES `profile` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
--- drugstore/src/main/resources/db/insert-institute.sql
+++ drugstore/src/main/resources/db/insert-institute.sql
@@ -232,18 +232,20 @@ insert into `institute`.`role` values (18, 'AGREEMENT_TRANSFER', 'Transferencia 
 insert into `institute`.`role` values (19, 'SUPPLYING', 'Dispensa');
 insert into `institute`.`role` values (20, 'SUPPLYING_CANCELLATION', 'Anulación de Dispensa');
 
-insert into `institute`.`user` values (1, "admin", "fad198c1bfe1cc7052905de0fa0431b45ec10ca4", 1);
+insert into profile_role values (1, 1, 1);
+insert into profile_role values (2, 1, 2);
+insert into profile_role values (12, 1, 12);
+insert into profile_role values (13, 1, 13);
+insert into profile_role values (15, 1, 15);
+insert into profile_role values (16, 1, 16);
+insert into profile_role values (17, 1, 17);
+insert into profile_role values (18, 1, 18);
+insert into profile_role values (19, 1, 19);
+insert into profile_role values (20, 1, 20);
 
-insert into user_role values (1, 1, 1);
-insert into user_role values (2, 1, 2);
-insert into user_role values (12, 1, 12);
-insert into user_role values (13, 1, 13);
-insert into user_role values (15, 1, 15);
-insert into user_role values (16, 1, 16);
-insert into user_role values (17, 1, 17);
-insert into user_role values (18, 1, 18);
-insert into user_role values (19, 1, 19);
-insert into user_role values (20, 1, 20);
+insert into `drugstore`.`profile` values (1, "admin");
+
+insert into `institute`.`user` values (1, "admin", "fad198c1bfe1cc7052905de0fa0431b45ec10ca4", 1,1);
 
 insert into audit_action values (1, 'Confirmado');
 insert into audit_action values (2, 'Modificado');
