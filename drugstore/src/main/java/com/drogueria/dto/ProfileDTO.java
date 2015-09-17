4af950d9e57d07a213524148dc531a09e25fef7c
luchopal <luchopal@gmail.com>
2015-09-17 16:49:25 -0300

    Se crea la clase ProfileDTO.java

---
+++ drugstore/src/main/java/com/drogueria/dto/ProfileDTO.java
@@ -0,0 +1,35 @@
+package com.drogueria.dto;
+
+import java.util.List;
+
+public class ProfileDTO {
+
+    private Integer id;
+    private String description;
+    private List<Integer> roles;
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<Integer> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(List<Integer> roles) {
+        this.roles = roles;
+    }
+}
