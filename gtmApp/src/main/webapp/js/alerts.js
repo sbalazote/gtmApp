c8496022d7e04e67442651e33825150617fe4744
luchopal <luchopal@gmail.com>
2015-11-24 11:41:36 -0300

    Se corrige f5 en firefox para dispensa.

--- gtmApp/src/main/webapp/js/alerts.js
+++ gtmApp/src/main/webapp/js/alerts.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
 			"message": message
 		};
 		sessionStorage.setItem("postSaveMessage", JSON.stringify(postObject));
-		window.location.reload(true);
+		window.location = window.location;
 	};
 	
 	myRedirect = function(type, message, redirectLocation) {
