123e7c5f99dfe65ae8650306f0962656ffb5a7e4
luchopal <luchopal@gmail.com>
2016-02-05 10:08:41 -0300

    Se modifica el alter para que sea generico.

--- gtmApp/src/main/resources/db/alter_tables/2016-02-05-alter-output-logistic-operator.sql
+++ gtmApp/src/main/resources/db/alter_tables/2016-02-05-alter-output-logistic-operator.sql
@@ -1,4 +1,4 @@
-ALTER TABLE drugstore.output ADD logistics_operator_id INT(11) NULL;
-ALTER TABLE drugstore.output
+ALTER TABLE output ADD logistics_operator_id INT(11) NULL;
+ALTER TABLE output
 ADD CONSTRAINT fk_output_logistics_operator
 FOREIGN KEY (logistics_operator_id) REFERENCES logistics_operator (id);
