1ab85413f6134ddc467736570cb3dbb17ebf1458
luchopal <lucho005>
2017-01-09 11:40:06 -0300

    Cambio de atributos en el schema principal y se corrige el alter para que funcione en MySQL 5.1

--- gtmApp/src/main/resources/db/alter_tables/2016-11-09-alter-client-affiliate-v1.0.3.sql
+++ gtmApp/src/main/resources/db/alter_tables/2016-11-09-alter-client-affiliate-v1.0.3.sql
@@ -1,13 +1,34 @@
-ALTER TABLE drugstore_dev.affiliate ADD sex VARCHAR(1) NULL;
-ALTER TABLE drugstore_dev.affiliate ADD address VARCHAR(200) NULL;
-ALTER TABLE drugstore_dev.affiliate ADD locality VARCHAR(250) NULL;
-ALTER TABLE drugstore_dev.affiliate ADD number VARCHAR(10) NULL;
-ALTER TABLE drugstore_dev.affiliate ADD floor VARCHAR(5) NULL;
-ALTER TABLE drugstore_dev.affiliate ADD apartment VARCHAR(5) NULL;
-ALTER TABLE drugstore_dev.affiliate ADD zip_code VARCHAR(8) NULL;
-ALTER TABLE drugstore_dev.affiliate ADD phone VARCHAR(30) NULL;
+ALTER TABLE institute.affiliate ADD sex VARCHAR(1) NULL;
+ALTER TABLE institute.affiliate ADD address VARCHAR(200) NULL;
+ALTER TABLE institute.affiliate ADD locality VARCHAR(250) NULL;
+ALTER TABLE institute.affiliate ADD number VARCHAR(10) NULL;
+ALTER TABLE institute.affiliate ADD floor VARCHAR(5) NULL;
+ALTER TABLE institute.affiliate ADD apartment VARCHAR(5) NULL;
+ALTER TABLE institute.affiliate ADD zip_code VARCHAR(8) NULL;
+ALTER TABLE institute.affiliate ADD phone VARCHAR(30) NULL;
 
-ALTER TABLE drugstore_dev.client_affiliate ADD associate_number VARCHAR(30) NULL;
+ALTER TABLE institute.client_affiliate ADD associate_number VARCHAR(30) NULL;
 
-ALTER TABLE drugstore_dev.affiliate CHANGE id affiliate_id INT(11) NOT NULL AUTO_INCREMENT;
-ALTER TABLE drugstore_dev.client CHANGE id client_id INT(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE institute.client_affiliate DROP FOREIGN KEY `fk_client_affiliate_affiliate`;
+ALTER TABLE institute.provisioning_request DROP FOREIGN KEY `fk_provisioning_request_affiliate`;
+ALTER TABLE institute.supplying DROP FOREIGN KEY `fk_supplying_affiliate_idx`;
+
+ALTER TABLE institute.client_affiliate DROP FOREIGN KEY `fk_client_affiliate_client`;
+ALTER TABLE institute.client_delivery_location DROP FOREIGN KEY `fk_client_delivery_location_client`;
+ALTER TABLE institute.provisioning_request DROP FOREIGN KEY `fk_provisioning_request_client`;
+ALTER TABLE institute.supplying DROP FOREIGN KEY `fk_supplying_client_idx`;
+
+ALTER TABLE institute.affiliate CHANGE id affiliate_id INT(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE institute.client CHANGE id client_id INT(11) NOT NULL AUTO_INCREMENT;
+
+CREATE INDEX affiliate_id_idx ON institute.affiliate (affiliate_id);
+CREATE INDEX client_id_idx ON institute.client (client_id);
+
+ALTER TABLE institute.client_affiliate ADD CONSTRAINT `fk_client_affiliate_affiliate` FOREIGN KEY (`affiliate_id`) REFERENCES `affiliate`(affiliate_id) ON DELETE NO ACTION ON UPDATE NO ACTION ;
+ALTER TABLE institute.provisioning_request ADD CONSTRAINT `fk_provisioning_request_affiliate` FOREIGN KEY (`affiliate_id`) REFERENCES `affiliate`(affiliate_id) ON DELETE NO ACTION ON UPDATE NO ACTION;
+ALTER TABLE institute.supplying ADD CONSTRAINT `fk_supplying_affiliate_idx` FOREIGN KEY (`affiliate_id`) REFERENCES `affiliate`(affiliate_id) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+ALTER TABLE institute.client_affiliate ADD CONSTRAINT `fk_client_affiliate_client` FOREIGN KEY (`client_id`) REFERENCES `client`(client_id) ON DELETE NO ACTION ON UPDATE NO ACTION;
+ALTER TABLE institute.client_delivery_location ADD CONSTRAINT `fk_client_delivery_location_client` FOREIGN KEY (`client_id`) REFERENCES `client`(client_id) ON DELETE NO ACTION ON UPDATE NO ACTION;
+ALTER TABLE institute.provisioning_request ADD CONSTRAINT `fk_provisioning_request_client` FOREIGN KEY (`client_id`) REFERENCES `client`(client_id) ON DELETE NO ACTION ON UPDATE NO ACTION;
+ALTER TABLE institute.supplying ADD CONSTRAINT `fk_supplying_client_idx` FOREIGN KEY (`client_id`) REFERENCES `client`(client_id) ON DELETE NO ACTION ON UPDATE NO ACTION;
